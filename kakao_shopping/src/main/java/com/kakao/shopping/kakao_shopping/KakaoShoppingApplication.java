de21074ca94fe347eb92906ba3abd58711204f5b
SeokjunMoon <seokjun0915@icloud.com>
2023-07-04 12:00:01 +0900

    feat: 프로젝트 생성
    - 카카오톡 쇼핑하기 클론을 위한 프로젝트 생성

---
+++ kakao_shopping/src/main/java/com/kakao/shopping/kakao_shopping/KakaoShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.shopping.kakao_shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoShoppingApplication.class, args);
+	}
+
+}
