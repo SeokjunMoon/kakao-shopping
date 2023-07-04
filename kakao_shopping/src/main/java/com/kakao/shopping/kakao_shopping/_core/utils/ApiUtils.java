e906c36912243e92094d8c498728be7bcaed7930
SeokjunMoon <seokjun0915@icloud.com>
2023-07-04 12:03:31 +0900

    feat: 유틸리티 추가
    - 응답을 위한 유틸리티 메소드 추가

---
+++ kakao_shopping/src/main/java/com/kakao/shopping/kakao_shopping/_core/utils/ApiUtils.java
@@ -0,0 +1,33 @@
+package com.example.kakao_shopping._core.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+public class ApiUtils {
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class ApiResult<T> {
+        final private boolean success;
+        final private T response;
+        final private ApiError<T> error;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class ApiError<T> {
+        final private String message;
+        final private int status;
+    }
+
+    public static <T> ApiResult<T> success(T response) {
+        return new ApiResult<>(true, response, null);
+    }
+
+    public static ApiResult<?> error (String message, HttpStatus httpStatus) {
+        return new ApiResult<>(false, null, new ApiError<>(message, httpStatus.value()));
+    }
+}
