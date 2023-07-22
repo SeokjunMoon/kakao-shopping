2afac76641094b813a4bd8d21cc893770e0e091e
SeokjunMoon <seokjun0915@icloud.com>
2023-07-22 14:33:49 +0900

    feat: ProductController 구현
    - 상품 삽입, 조회 결과 전달을 위한 컨트롤러 생성

--- kakao-shopping/src/main/java/com/kakao/shopping/controller/ProductController.java
+++ kakao-shopping/src/main/java/com/kakao/shopping/controller/ProductController.java
@@ -1,22 +1,31 @@
 package com.kakao.shopping.controller;
 
 import com.kakao.shopping._core.utils.ApiUtils;
+import com.kakao.shopping.dto.product.ProductDTO;
+import com.kakao.shopping.dto.product.ProductListItemDTO;
+import com.kakao.shopping.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
-@RequestMapping("/products")
 public class ProductController {
-    @GetMapping("/")
-    public ResponseEntity<?> findAll() {
-        return ResponseEntity.ok().body(ApiUtils.success(null));
+    private final ProductService productService;
+
+    @GetMapping("/products")
+    public ResponseEntity<?> findAll(@RequestParam(defaultValue = "0") int page) {
+        PageRequest pageRequest = PageRequest.of(page, 9);
+        List<ProductListItemDTO> products = productService.findAllProducts(pageRequest);
+        return ResponseEntity.ok().body(ApiUtils.success(products));
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable int id) {
-        return ResponseEntity.ok(ApiUtils.success(null));
+    @GetMapping("/products/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        ProductDTO product = productService.findProductById(id);
+        return ResponseEntity.ok(ApiUtils.success(product));
     }
 }
