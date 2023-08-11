90256d7c55c3275498b2028dabb26a3082b3981b
SeokjunMoon <seokjun0915@icloud.com>
2023-08-11 16:23:35 +0900

    refactor: 불필요한 코드 삭제
    - 불필요한 import 삭제

--- kakao-shopping/src/main/java/com/kakao/shopping/controller/UserAccountController.java
+++ kakao-shopping/src/main/java/com/kakao/shopping/controller/UserAccountController.java
@@ -1,6 +1,5 @@
 package com.kakao.shopping.controller;
 
-import com.kakao.shopping._core.errors.exception.BadRequestException;
 import com.kakao.shopping._core.security.JwtTokenProvider;
 import com.kakao.shopping._core.utils.ApiUtils;
 import com.kakao.shopping.dto.user.UserLoginRequest;
@@ -8,22 +7,18 @@ import com.kakao.shopping.dto.user.UserRegisterRequest;
 import com.kakao.shopping.service.UserAccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.Errors;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.InvalidPropertiesFormatException;
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class UserAccountController {
     private final UserAccountService userAccountService;
-    private final PasswordEncoder passwordEncoder;
 
     @PostMapping("/join")
     public ResponseEntity<?> join(@RequestBody UserRegisterRequest userRegisterRequest, Errors error, HttpServletRequest request) throws InvalidPropertiesFormatException {
