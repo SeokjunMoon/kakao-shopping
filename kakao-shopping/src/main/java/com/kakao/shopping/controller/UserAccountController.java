d7c67d09b32651f571732bd00961b65518d178b1
SeokjunMoon <seokjun0915@icloud.com>
2023-08-11 16:22:14 +0900

    refactor: UserAccountController
    - GlobalHandler로 예외 처리

--- kakao-shopping/src/main/java/com/kakao/shopping/controller/UserAccountController.java
+++ kakao-shopping/src/main/java/com/kakao/shopping/controller/UserAccountController.java
@@ -27,36 +27,16 @@ public class UserAccountController {
 
     @PostMapping("/join")
     public ResponseEntity<?> join(@RequestBody UserRegisterRequest userRegisterRequest, Errors error, HttpServletRequest request) throws InvalidPropertiesFormatException {
-        ResponseEntity<? extends ApiUtils.ApiResult<?>> exception = checkErrors(error);
-        if (exception != null) return exception;
-
         userAccountService.register(userRegisterRequest);
         return ResponseEntity.ok().body(ApiUtils.success(null));
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody UserLoginRequest loginRequest, Errors error, HttpServletRequest request) {
-        ResponseEntity<? extends ApiUtils.ApiResult<?>> exception = checkErrors(error);
-        if (exception != null) return exception;
-
         String jwtToken = userAccountService.login(loginRequest);
         return ResponseEntity
                 .ok()
                 .header(JwtTokenProvider.HEADER, jwtToken)
                 .body(ApiUtils.success(null));
-    }
-
-    private static ResponseEntity<? extends ApiUtils.ApiResult<?>> checkErrors(Errors error) {
-        if (error.hasErrors()) {
-            List<FieldError> errors = error.getFieldErrors();
-            BadRequestException exception = new BadRequestException(
-                    errors.get(0).getDefaultMessage() + " : " + errors.get(0).getField()
-            );
-            return new ResponseEntity<>(
-                    exception.body(),
-                    exception.status()
-            );
-        }
-        return null;
     }
 }
