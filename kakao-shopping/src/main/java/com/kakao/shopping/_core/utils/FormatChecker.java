aa090938e218e1045455ae4767e506838c4db896
SeokjunMoon <seokjun0915@icloud.com>
2023-08-12 09:02:22 +0900

    feat: FormatChecker
    - 로그인 및 회원가입 요청 정보의 유효성 검증을 위한 객체 분리

---
+++ kakao-shopping/src/main/java/com/kakao/shopping/_core/utils/FormatChecker.java
@@ -0,0 +1,72 @@
+package com.kakao.shopping._core.utils;
+
+import com.kakao.shopping._core.errors.exception.BadRequestException;
+import com.kakao.shopping._core.errors.exception.InvalidFormatException;
+
+public class FormatChecker {
+    enum TYPE {REGISTER, LOGIN};
+    private final String name;
+    private final String email;
+    private final String password;
+    private final TYPE type;
+
+    public FormatChecker(String name, String email, String password) {
+        this.type = TYPE.REGISTER;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public FormatChecker(String email, String password) {
+        this.type = TYPE.LOGIN;
+        this.email = email;
+        this.name = null;
+        this.password = password;
+    }
+
+    public void execute() {
+        checkEmailFormat();
+        checkPasswordFormat();
+
+        if (this.type == TYPE.REGISTER) {
+            checkNameFormat();
+        }
+    }
+
+    private void checkNameFormat() {
+        if (this.name == null) {
+            throw new BadRequestException("이름을 입력해주세요.");
+        }
+        if (this.name.length() > 45) {
+            throw new InvalidFormatException("이름의 길이는 45자 이하만 가능합니다.");
+        }
+    }
+
+    private void checkEmailFormat() {
+        if (this.email == null) {
+            throw new BadRequestException("이메일을 입력해주세요.");
+        }
+        if (this.email.length() < 1 || this.email.length() > 100) {
+            throw new InvalidFormatException("이메일은 1자 이상 100자 이하만 가능합니다.");
+        }
+
+        String regex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        if (!this.email.matches(regex)) {
+            throw new InvalidFormatException("이메일 형식이 잘못되었습니다.");
+        }
+    }
+
+    private void checkPasswordFormat() {
+        if (this.password == null) {
+            throw new BadRequestException("비밀번호를 입력해주세요.");
+        }
+        if (this.password.length() < 8 || this.password.length() > 256) {
+            throw new InvalidFormatException("비밀번호의 길이는 8 이상 256 이하만 가능합니다.");
+        }
+
+        String regex = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$";
+        if (!this.password.matches(regex)) {
+            throw new InvalidFormatException("비밀번호는 1개 이상의 영문자, 1개 이상의 숫자, 1개 이상의 특수문자를 포함해야 합니다.");
+        }
+    }
+}
--- kakao-shopping/src/main/java/com/kakao/shopping/service/UserAccountService.java
+++ kakao-shopping/src/main/java/com/kakao/shopping/service/UserAccountService.java
@@ -1,9 +1,9 @@
 package com.kakao.shopping.service;
 
 import com.kakao.shopping._core.errors.exception.BadRequestException;
-import com.kakao.shopping._core.errors.exception.InvalidFormatException;
 import com.kakao.shopping._core.security.CustomUserDetails;
 import com.kakao.shopping._core.security.JwtTokenProvider;
+import com.kakao.shopping._core.utils.FormatChecker;
 import com.kakao.shopping.domain.UserAccount;
 import com.kakao.shopping.dto.user.UserLoginRequest;
 import com.kakao.shopping.dto.user.UserRegisterRequest;
@@ -30,9 +30,8 @@ public class UserAccountService implements UserDetailsService {
     }
 
     public UserAccount register(UserRegisterRequest request) {
-        checkEmailFormat(request.email());
-        checkNameFormat(request.name());
-        checkPasswordFormat(request.password());
+        FormatChecker formatChecker = new FormatChecker(request.name(), request.email(), request.password());
+        formatChecker.execute();
 
         try {
             return userAccountRepository.save(
@@ -50,6 +49,9 @@ public class UserAccountService implements UserDetailsService {
     }
 
     public String login(UserLoginRequest request) {
+        FormatChecker formatChecker = new FormatChecker(request.email(), request.password());
+        formatChecker.execute();
+
         UserAccount userAccount = userAccountRepository.findByEmail(request.email())
                 .orElseThrow(() -> new BadRequestException("등록되지 않은 이메일 입니다."));
 
@@ -58,35 +60,5 @@ public class UserAccountService implements UserDetailsService {
         }
 
         return JwtTokenProvider.create(userAccount);
-    }
-
-    // --------------------------------------------------------------------------------
-
-    private void checkEmailFormat(String email) {
-        if (email == null || email.length() < 1 || email.length() > 100) {
-            throw new InvalidFormatException("이메일은 1자 이상 100자 이하만 가능합니다.");
-        }
-
-        String regex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
-        if (!email.matches(regex)) {
-            throw new InvalidFormatException("이메일 형식이 잘못되었습니다.");
-        }
-    }
-
-    private void checkNameFormat(String name) {
-        if (name.length() > 45) {
-            throw new InvalidFormatException("이름의 길이는 45자 이하만 가능합니다.");
-        }
-    }
-
-    private void checkPasswordFormat(String password) {
-        if (password == null || password.length() < 8 || password.length() > 256) {
-            throw new InvalidFormatException("비밀번호의 길이는 8 이상 256 이하만 가능합니다.");
-        }
-
-        String regex = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$";
-        if (!password.matches(regex)) {
-            throw new InvalidFormatException("비밀번호는 1개 이상의 영문자, 1개 이상의 숫자, 1개 이상의 특수문자를 포함해야 합니다.");
-        }
     }
 }
