8795d4c9a0b33f1a159099327c31f38bb58a6273
SeokjunMoon <seokjun0915@icloud.com>
2023-08-13 15:40:59 +0900

    feat: OptionRepository
    - findAllyId의 lazy loading 패치 추가

--- kakao-shopping/src/main/java/com/kakao/shopping/repository/OptionRepository.java
+++ kakao-shopping/src/main/java/com/kakao/shopping/repository/OptionRepository.java
@@ -3,6 +3,7 @@ package com.kakao.shopping.repository;
 import com.kakao.shopping.domain.ProductOption;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +15,10 @@ public interface OptionRepository extends JpaRepository<ProductOption, Long> {
 
     @EntityGraph("OptionWithProduct")
     List<ProductOption> findAllByProductId(Long productId);
+
+    @Query("select o " +
+            "from ProductOption o " +
+            "join fetch o.product " +
+            "where o.id in (:ids)")
+    List<ProductOption> findAllById(List<Long> ids);
 }
